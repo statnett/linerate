1f6fbe071fe8726d670e13a82c17b38cded7018b
Amund Faller Råheim <amundfr@gmail.com>
2024-04-03 15:18:03 +0200

    test that bisect raises ValueError

--- tests/test_solver.py
+++ tests/test_solver.py
@@ -31,19 +31,16 @@ def test_compute_conductor_temperature_computes_correct_ampacity():
     assert conductor_temperature == pytest.approx(9000, rel=1e-7)
 
 
-def test_compute_conductor_temperature_raises_value_error():
-    def heat_balance(conductor_temperature, current):
+def test_bisect_raises_value_error():
+    def heat_balance(current):
         A = current
-        T = conductor_temperature
-        return (A + 100 * T) * (current + 200 * T)
+        T = 90
+        return (A + 100 * T) * (A + 100 * T)
 
-    try:
-        solver.compute_conductor_ampacity(
+    with pytest.raises(ValueError):
+        solver.bisect(
             heat_balance,
-            max_conductor_temperature=90,
-            min_ampacity=0,
-            max_ampacity=10_000,
+            xmin=0,
+            xmax=10_000,
             tolerance=1e-8,
         )
-    except Exception as e:
-        assert isinstance(e, ValueError)
