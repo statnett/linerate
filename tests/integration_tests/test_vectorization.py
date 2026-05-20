934b0455d8562af9758a90508011a171b3168670
Erkko Ihalainen <erkko.Ihalainen@fingrid.fi>
2026-05-20 12:57:36 +0300

    Made vectorization tests more similar and increased maximum number of dimensions to 5.

--- tests/integration_tests/test_vectorization.py
+++ tests/integration_tests/test_vectorization.py
@@ -1,5 +1,4 @@
 import hypothesis
-import hypothesis.strategies as st
 from hypothesis.extra.numpy import mutually_broadcastable_shapes, BroadcastableShapes
 import numpy as np
 import numpy.typing as npt
@@ -7,7 +6,8 @@ import numpy.typing as npt
 from linerate.models.cigre601 import Cigre601
 from linerate.types import Conductor, ConductorWithTransientData, Span, Tower, Weather
 
-NUMBER_OF_VARIABLES = 33
+NUMBER_OF_VARIABLES_TRANSIENT = 33
+NUMBER_OF_VARIABLES_STEADY_STATE = 24
 
 
 class Shapes:
@@ -22,67 +22,56 @@ class Shapes:
 
 
 @hypothesis.given(
-    vectorization_indices=st.sets(st.integers(min_value=1, max_value=24), max_size=10)
+    array_shapes=mutually_broadcastable_shapes(num_shapes=NUMBER_OF_VARIABLES_STEADY_STATE, max_dims=5)
 )
-def test_vectorization(vectorization_indices: set[int]):
-    def get_zeros(index, length) -> npt.NDArray[np.floating]:
-        out = np.ones(length, dtype=int)
-        if index in vectorization_indices:
-            out[index] = 2
-        return np.zeros(out, dtype=np.dtype("f8"))
+def test_vectorization(array_shapes: BroadcastableShapes):
+    shapes = Shapes(array_shapes.input_shapes)
 
     conductor = Conductor(
-        core_diameter=get_zeros(0, 25) + 1,
-        conductor_diameter=get_zeros(1, 25) + 2,
-        outer_layer_strand_diameter=get_zeros(2, 25),
-        emissivity=get_zeros(3, 25),
-        solar_absorptivity=get_zeros(4, 25),
-        temperature1=get_zeros(5, 25),
-        temperature2=get_zeros(6, 25) + 1,
-        resistance_at_temperature1=get_zeros(7, 25),
-        resistance_at_temperature2=get_zeros(8, 25),
-        aluminium_cross_section_area=1 + get_zeros(9, 25),
-        constant_magnetic_effect=get_zeros(10, 25),
-        current_density_proportional_magnetic_effect=get_zeros(11, 25) + 1,
-        max_magnetic_core_relative_resistance_increase=get_zeros(12, 25),
+        core_diameter=1,
+        conductor_diameter=shapes.get_zeros() + 2,
+        outer_layer_strand_diameter=shapes.get_zeros(),
+        emissivity=shapes.get_zeros(),
+        solar_absorptivity=shapes.get_zeros(),
+        temperature1=shapes.get_zeros(),
+        temperature2=shapes.get_zeros() + 1,
+        resistance_at_temperature1=shapes.get_zeros(),
+        resistance_at_temperature2=shapes.get_zeros(),
+        aluminium_cross_section_area=1 + shapes.get_zeros(),
+        constant_magnetic_effect=shapes.get_zeros(),
+        current_density_proportional_magnetic_effect=shapes.get_zeros() + 1,
+        max_magnetic_core_relative_resistance_increase=shapes.get_zeros(),
     )
     weather = Weather(
-        air_temperature=get_zeros(13, 25),
-        wind_direction=get_zeros(14, 25),
-        wind_speed=get_zeros(15, 25),
-        ground_albedo=get_zeros(23, 25),
-        clearness_ratio=get_zeros(16, 25),
+        air_temperature=shapes.get_zeros(),
+        wind_direction=shapes.get_zeros(),
+        wind_speed=shapes.get_zeros(),
+        ground_albedo=shapes.get_zeros(),
+        clearness_ratio=shapes.get_zeros(),
     )
     span = Span(
         conductor=conductor,
         start_tower=Tower(
-            get_zeros(17, 25),
-            get_zeros(18, 25),
-            get_zeros(19, 25),
+            shapes.get_zeros(),
+            shapes.get_zeros(),
+            shapes.get_zeros(),
         ),
         end_tower=Tower(
-            0.1 + get_zeros(20, 25),
-            0.1 + get_zeros(21, 25),
-            10 + get_zeros(22, 25),
+            0.1 + shapes.get_zeros(),
+            0.1 + shapes.get_zeros(),
+            10 + shapes.get_zeros(),
         ),
         num_conductors=1,
     )
-    time = np.array([np.datetime64("2022-01-01")]).reshape([1] * 25)
-    if 24 in vectorization_indices:
-        time = np.concatenate([time, time], axis=-1)
+    time = shapes.get_zeros(dtype=np.dtype("datetime64[D]"))
 
     model = Cigre601(span=span, weather=weather, time=time)
     temperature = model.compute_conductor_temperature(100)
-    shape = [1] * 25
-    for vectorisation_index in vectorization_indices:
-        shape[vectorisation_index] = 2
-    shape[0] = 1
-
     assert isinstance(temperature, np.ndarray)
-    assert temperature.shape == tuple(shape)
+    assert temperature.shape == array_shapes.result_shape
 
 
-@hypothesis.given(array_shapes=mutually_broadcastable_shapes(num_shapes=NUMBER_OF_VARIABLES))
+@hypothesis.given(array_shapes=mutually_broadcastable_shapes(num_shapes=NUMBER_OF_VARIABLES_TRANSIENT, max_dims=5))
 def test_transient_vectorization(array_shapes: BroadcastableShapes):
     shapes = Shapes(array_shapes.input_shapes)
 
